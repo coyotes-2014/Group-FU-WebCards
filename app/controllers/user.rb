3f12c38908287f96c54dedb0e831c5b858978b9a
Martin Lear & Natalie McCroy <pair+mlear.nataliemac81@devbootcamp.com>
2014-06-27 14:14:09 -0500

    updates

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -1,7 +1,8 @@
 post '/user/:id/deck/:deck_id' do
   if logged_in?
     @user = current_user
-    @deck = Deck.find_by(id: params[:deck_id])
+    session[:deck] = Deck.find_by(id: params[:deck_id])
+    @deck = session[:deck]
     @user_deck = User_deck.new(user_id: @user.id, deck_id: @deck.id)
     erb :"/user/show_card"
   else
@@ -11,21 +12,22 @@ end
 
 post '/user/:id/deck/:deck_id/done' do
   @user = current_user
-  @deck = User_deck.find_by(id: params[:deck_id])
-  cards_left = @user_deck.where(cards.answered: false)
+  @deck = session[:deck]
+  cards_left = @deck.cards.where(answered: false)
   result = (@user_deck.cards.size - cards_left)/@user_deck.cards.size
   User_deck.update(@user_deck.id, user_result: result)
+  Card.update_all(answered: false)
   erb :"/user/results"
 end
 
 post '/user/:id/deck/:deck_id/guess' do
   @user = current_user
-  @deck = User_deck.find_by(id: params[:user_deck_id])
+  @deck = session[:deck]
   @card = Card.find_by(id: params[:card_id])
   if params[:guess] == @card.definition
     Card.update(@card.id, answered: true)
-    erb :"/user/#{@user.id}/deck/#{@deck.id}/show_card"
+    erb :"/user/deck/show_card"
   else
-    erb :"/user/show_card/incorrect"
+    erb :"/user/incorrect"
   end
 end
